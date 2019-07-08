a3c7c0215d782b107614fe5bfef4fef1dc432816
Deepak Chauhan <deepakchauhan878@gmail.com>
2019-07-08 15:24:19 +0000

    Update get_hw_info.py

--- get_hw_info.py
+++ get_hw_info.py
@@ -2,7 +2,23 @@ import os
 from tabulate import tabulate
 
 class get_hw_info:
+	'''
+	get_hw_info HAVE A SINGLE METHOD AND A CONSTRUCTOR FUNCTION WHICH ARE NAMED AS :
+		1) __init__
+		2) work()
+			__init__ DOCFILE:
+				__init__ CONTAINS INITIALISED AND UNINITIALISED VARIABLES FOR LATER USE BY CLASS METHODS. 
+			WORK() DOCFILE:
+				work() RETURN A DATA VARIABLE CONTAINING GIVEN DATA :
+					1) BASIC INFORMATION 
+					2) MEMORY STATISTICS
+					3) INSTALLED DRIVERS LIST 
+	'''
 	def __init__(self):
+		'''
+		__init__ DOCFILE:
+			__init__ CONTAINS INITIALISED AND UNINITIALISED VARIABLES FOR LATER USE BY CLASS METHODS. 
+		'''
 		self.mem_info = ""																							# TO SAVE MEMORY INFO
 		self.drivers = []																							# TO SAVE LIST OF INSTALLED DRIVERS
 		self.drivers_data = []																						# TO SAVE MODIFIED DATA INTO A SEPERATE LIST
@@ -11,6 +27,14 @@ class get_hw_info:
 		self.data = ""																								# TO SAVE THE FINALIZED DATA TO BE RETURNED	
 
 	def work(self):
+		'''
+		WORK() DOCFILE:
+			work() RETURN A DATA VARIABLE CONTAINING GIVEN DATA :
+				1) BASIC INFORMATION 
+				2) MEMORY STATISTICS
+				3) INSTALLED DRIVERS LIST 
+		'''
+
 		# CPU INFO
 		self.cpu_info = os.popen("lscpu | grep -e 'Model name' -e 'Architecture'").read().split('\n')				# COOLLECTING CPU INFO AND SAVING IT IN A LIST
 		(self.cpu_info[0], self.cpu_info[1]) = (self.cpu_info[1], self.cpu_info[0])									# REARRANGING DATA
@@ -46,31 +70,33 @@ class get_hw_info:
 
 
 		# MEMORY INFO
-		self.mem_info = os.popen("free").read().split('\n')
-		self.mem_info = [mem.split(" ") for mem in self.mem_info]
+		self.mem_info = os.popen("free").read().split('\n')															# SAVING MEMORY STATS INTO LIST
+		self.mem_info = [mem.split(" ") for mem in self.mem_info]													# SUBLISTING THE ELEMENTS IN LIST
 
-		for mem in self.mem_info:
+		for mem in self.mem_info:																					# REMOVING REDUNDANT ELEMENTS FROM LIST
 			try:
 				while True:
 					mem.remove('')
 			except Exception as e:
 				pass
-		self.mem_info.pop()
-		self.mem_info[0].insert(0, 'Memory Type')
+		self.mem_info.pop()																							# REMOVING LAST REDUNDANT ELEMENT
+		self.mem_info[0].insert(0, 'Memory Type')																	# INSERTNG NEW HEADER ELEMENT AT START OF LIST
 
-		for mem in self.mem_info[1:]:
+		for mem in self.mem_info[1:]:																				# CONVERTING kB DATA TO gB AND ADDING GB AT END OF MEMORY STAT
 			for m in range(1,len(mem)):
 				 mem[m] = str(int(mem[m])/1000000) + " GB"
 
-		for mem in self.mem_info:
+		for mem in self.mem_info:																					# ADDING - AT MISSING DATA
 			if len(mem) <= len(self.mem_info[0]):
 				for i in range(0, len(self.mem_info[0]) - len(mem)):
 					mem.append('-')
 
 		# RAM SIZE
-		self.ram_size = self.mem_info[1][1]
-		self.cpu_info.append(["Installed RAM", self.ram_size])
+		self.ram_size = self.mem_info[1][1]																			# COLLECTING INSTALLED MEMORY INFO FROM MEMORY STATS
+		self.cpu_info.append(["Installed RAM", self.ram_size])														# ADDING THIS DATA INTO LIST CONTAINIMG BASIC DETAILS
 
+
+		# SAVING DATA INTO A DATA VARIABLE WHICH CAN BE RETURNED LATER
 		self.data += "-------------------- BASIC INFORMATION --------------------\n"
 		self.data += tabulate(self.cpu_info, headers=['PROPERTY', 'VALUE'])
 		self.data += "\n\n\n--------------------------------------- MEMORY STATS ---------------------------------------\n"
@@ -78,5 +104,5 @@ class get_hw_info:
 		self.data += "\n\n\n-------------- DRIVERS INSTALLED --------------\n"
 		self.data += tabulate(self.drivers_data, headers=['LIST 1','LIST 2','LIST 3','LIST 4'])
 
+		# RETURNING DATA VARIABLE
 		return self.data
-
